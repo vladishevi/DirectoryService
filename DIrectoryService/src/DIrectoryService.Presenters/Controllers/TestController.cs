71c23bd415dcd519b070e8487277a87ff7814137
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-13 20:41:28 +0100

    add testController

---
+++ DIrectoryService/src/DIrectoryService.Presenters/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DirectoryService.Presenters.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+internal class TestController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult GetTest()
+    {
+        return Ok("testOk");
+    }
+}
