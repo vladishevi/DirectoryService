437c8a68f11bdcda236d83330835f896c85545fb
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-16 17:08:03 +0100

    create Name VO

--- DIrectoryService/src/DirectoryService.Domain/Departments/Department.cs
+++ DIrectoryService/src/DirectoryService.Domain/Departments/Department.cs
@@ -3,7 +3,7 @@
 public class Department
 {
     public Guid Id { get; private set; }
-    public string Name { get; private set; }
+    public Name Name { get; private set; }
     public string Identifier { get; private set; }
     public Guid? ParentId { get; private set; }
     public string Path { get; private set; }
---
+++ DIrectoryService/src/DirectoryService.Domain/Departments/Name.cs
@@ -0,0 +1,31 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace DirectoryService.Domain.Departments;
+
+public record Name
+{
+    private const int MIN_LENGHT = 3;
+    private const int MAX_LENGHT = 150;
+
+    public string Value { get; }
+    
+    private Name(string value)
+    {
+        Value = value;
+    }
+    
+    public static Result<Name, string> Create(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return  "Department name cannot be empty";
+        }
+        
+        if (value.Length is < MIN_LENGHT or > MAX_LENGHT)
+        {
+            return $"Department name must be between {MIN_LENGHT} and {MAX_LENGHT} characters";
+        }
+        
+        return new Name(value);
+    }
+}
