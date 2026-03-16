2f91fef7c63267fea9b297acda033974016cdc0d
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-16 16:48:52 +0100

    add initial department model

---
+++ DIrectoryService/src/DirectoryService.Domain/Departments/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace DirectoryService.Domain.Departments;
+
+public class Department
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Identifier { get; private set; }
+    public Guid? ParentId { get; private set; }
+    public string Path { get; private set; }
+    public short Depth { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt  { get; private set; }
+    public DateTime UpdatedAt  { get; private set; }
+}
