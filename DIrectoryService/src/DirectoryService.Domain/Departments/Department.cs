5cc8384bdbd25995d19c8ba5fdfff2efea2e4dab
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-18 14:05:02 +0100

    add identifier, name and path VOs

--- DIrectoryService/src/DirectoryService.Domain/Departments/Department.cs
+++ DIrectoryService/src/DirectoryService.Domain/Departments/Department.cs
@@ -2,13 +2,26 @@
 
 public class Department
 {
+    private Department(Name name, Identifier identifier, Department parentDepartment)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Identifier = identifier;
+        ParentDepartment = parentDepartment;
+    }
+    
     public Guid Id { get; private set; }
     public Name Name { get; private set; }
-    public string Identifier { get; private set; }
-    public Guid? ParentId { get; private set; }
+    public Identifier Identifier { get; private set; }
+    public Department? ParentDepartment { get; private set; }
     public string Path { get; private set; }
     public short Depth { get; private set; }
     public bool IsActive { get; private set; }
     public DateTime CreatedAt  { get; private set; }
     public DateTime UpdatedAt  { get; private set; }
+
+    public static Department Create(Name name, Identifier identifier, Department parentDepartment)
+    {
+        return new Department(name, identifier, parentDepartment);
+    }
 }
---
+++ DIrectoryService/src/DirectoryService.Domain/Departments/ValueObjects/Path.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using CSharpFunctionalExtensions;
+
+namespace DirectoryService.Domain.Departments;
+
+public record Path
+{
+    private Path(string value)
+    {
+        Value = value;
+    }
+    
+    public string Value { get; private set; }
+    
+    public static Result<Path, string> Create(string identifier, Department parentDepartment)
+    {
+        StringBuilder pathBuilder = new StringBuilder();
+        pathBuilder.Insert(0, identifier);
+
+        Department? parent = parentDepartment;
+        while (parent != null)
+        {
+            pathBuilder.Insert(0, parent.Identifier);
+            parent = parent.ParentDepartment;
+        }
+        
+        return new Path(pathBuilder.ToString());   
+    }
+}
