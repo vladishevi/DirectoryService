0b00a94e6f3ba37ac38a0e9d2d591b8136868a9b
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-20 11:12:43 +0100

    add position domain

---
+++ DIrectoryService/src/DirectoryService.Domain/Positions/ValueObjects/Description.cs
@@ -0,0 +1,25 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace DirectoryService.Domain.Positions;
+
+public record Description
+{
+    private const int MAX_LENGTH = 1000;
+    
+    public string Value { get; private set; }
+
+    private Description(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<Description, string> Create(string value)
+    {
+        if (value.Length > MAX_LENGTH)
+        {
+            return $"Description name must be no longer than {MAX_LENGTH}";
+        }
+
+        return new Description(value);
+    }
+}
---
+++ DIrectoryService/src/DirectoryService.Domain/Positions/ValueObjects/Name.cs
@@ -0,0 +1,36 @@
+﻿using CSharpFunctionalExtensions;
+
+namespace DirectoryService.Domain.Positions;
+
+public record Name
+{
+    private const int MIN_LENGHT = 3;
+    private const int MAX_LENGHT = 150;
+
+    public string Value { get; }
+    
+    private Name(string value)
+    {
+        Value = value;
+    }
+
+    public static Result<Name, string> Create(string value, IEnumerable<string> createdNames)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            return "Position name cannot be empty";
+        }
+
+        if (value.Length is < MIN_LENGHT or > MAX_LENGHT)
+        {
+            return $"Position name must be between {MIN_LENGHT} and {MAX_LENGHT} characters";
+        }
+
+        if (createdNames.Contains(value))
+        {
+            return "Position name must be unique";
+        }
+
+        return new Name(value);
+    }
+}
