aef1d7ad968160bb748807357452220f94fafde3
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-22 16:10:28 +0100

    fix misspell

--- DIrectoryService/src/DirectoryService.Domain/DepartmentLocations/DepartmentLocation.cs
+++ DIrectoryService/src/DirectoryService.Domain/DepartmentLocations/DepartmentLocation.cs
@@ -5,11 +5,11 @@ public class DepartmentLocation
     public DepartmentLocation(Guid departementId, Guid locationId)
     {
         Id = Guid.NewGuid();
-        DepartementId = departementId;
+        DepartmentId = departementId;
         LocationId = locationId;
     }
     
     public Guid Id { get; private set; }
-    public Guid DepartementId { get; private set; }
+    public Guid DepartmentId { get; private set; }
     public Guid LocationId { get; private set; }
 }
