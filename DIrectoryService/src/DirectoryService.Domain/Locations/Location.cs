286eb4484d58baafcfe515b2caeda7737c83b9bc
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-22 12:04:55 +0100

    add location updated, created and isActive properties

--- DIrectoryService/src/DirectoryService.Domain/Locations/Location.cs
+++ DIrectoryService/src/DirectoryService.Domain/Locations/Location.cs
@@ -10,10 +10,16 @@ public class Location
         Name = name;
         Address = address;
         Timezone = timezone;
+        IsActive = true;
+        CreatedAt = DateTime.UtcNow;
+        UpdatedAt = DateTime.UtcNow;
     }
     
     public Guid Id { get; private set; }
     public Name Name { get; private set; }
     public Address Address { get; private set; }
     public Timezone Timezone { get; private set; }
+    public bool IsActive { get; private set; }
+    public DateTime CreatedAt  { get; private set; }
+    public DateTime UpdatedAt  { get; private set; }
 }
