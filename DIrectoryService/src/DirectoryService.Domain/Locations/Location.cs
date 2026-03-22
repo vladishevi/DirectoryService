307c1a041de618eff84768fccdca56890e5a9a96
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-22 10:42:59 +0100

    add initial location class

--- DIrectoryService/src/DirectoryService.Domain/Locations/Location.cs
+++ DIrectoryService/src/DirectoryService.Domain/Locations/Location.cs
@@ -1,6 +1,15 @@
-﻿namespace DirectoryService.Domain.Locations;
+﻿using DirectoryService.Domain.Common;
+
+namespace DirectoryService.Domain.Locations;
 
 public class Location
 {
+    public Location(Name name)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+    }
     
+    public Guid Id { get; private set; }
+    public Name Name { get; private set; }
 }
