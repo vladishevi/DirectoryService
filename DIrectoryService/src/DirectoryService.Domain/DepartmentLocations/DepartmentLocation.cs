76e026b1e3a222efe618c2fa41b919a6c3dd4d48
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-22 12:19:38 +0100

    add departmentLocations and departmentPositions domains

---
+++ DIrectoryService/src/DirectoryService.Domain/DepartmentLocations/DepartmentLocation.cs
@@ -0,0 +1,13 @@
+﻿namespace DirectoryService.Domain.DepartmentLocations;
+
+public class DepartmentLocation
+{
+    public DepartmentLocation(Guid departementId, Guid locationId)
+    {
+        DepartementId = departementId;
+        LocationId = locationId;
+    }
+    
+    public Guid DepartementId { get; private set; }
+    public Guid LocationId { get; private set; }
+}
---
+++ DIrectoryService/src/DirectoryService.Domain/DepartmentPositions/DepartmentPosition.cs
@@ -0,0 +1,13 @@
+﻿namespace DirectoryService.Domain.DepartmentPositions;
+
+public class DepartmentPosition
+{
+    public DepartmentPosition(Guid departmentId, Guid positionId)
+    {
+        DepartmentId = departmentId;
+        PositionId = positionId;
+    }
+    
+    public Guid DepartmentId { get; private set; }
+    public Guid PositionId { get; private set; }
+}
