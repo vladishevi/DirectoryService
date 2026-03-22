191a1335bcd84f6b1eb85f37c5b5606cc11102a7
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-22 16:35:01 +0100

    add locations and positions list to the department domain

--- DIrectoryService/src/DirectoryService.Domain/Departments/Department.cs
+++ DIrectoryService/src/DirectoryService.Domain/Departments/Department.cs
@@ -1,4 +1,6 @@
 ﻿using DirectoryService.Domain.Common;
+using DirectoryService.Domain.DepartmentLocations;
+using DirectoryService.Domain.DepartmentPositions;
 
 namespace DirectoryService.Domain.Departments;
 
@@ -32,6 +34,18 @@ public class Department
     public DateTime CreatedAt  { get; private set; }
     public DateTime UpdatedAt  { get; private set; }
 
+    public IReadOnlyCollection<DepartmentLocation> Locations => _locations;
+    public IReadOnlyCollection<DepartmentPosition> Positions => _positions;
+    
+    private readonly List<DepartmentLocation> _locations = [];
+    private readonly List<DepartmentPosition> _positions = [];
+
+    public void AddLocation(Guid locationId) => 
+        _locations.Add(new DepartmentLocation(Id, locationId));
+    
+    public void AddPosition(Guid positionId) => 
+        _positions.Add(new DepartmentPosition(Id, positionId));
+    
     private static short CalcDepth(Department? parentDepartment)
     {
         short depth = 1;
