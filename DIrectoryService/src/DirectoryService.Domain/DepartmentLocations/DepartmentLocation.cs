0684280e302e249177f2bbcc0c64802fe9d7d3e3
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-22 14:57:08 +0100

    add guid to depLocs and depPoss

--- DIrectoryService/src/DirectoryService.Domain/DepartmentLocations/DepartmentLocation.cs
+++ DIrectoryService/src/DirectoryService.Domain/DepartmentLocations/DepartmentLocation.cs
@@ -4,10 +4,12 @@ public class DepartmentLocation
 {
     public DepartmentLocation(Guid departementId, Guid locationId)
     {
+        Id = Guid.NewGuid();
         DepartementId = departementId;
         LocationId = locationId;
     }
     
+    public Guid Id { get; private set; }
     public Guid DepartementId { get; private set; }
     public Guid LocationId { get; private set; }
 }
