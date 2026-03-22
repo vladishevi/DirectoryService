4aa35f77b7a4c35664cecc0b3c050af8c8fb048f
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-22 16:43:24 +0100

    refactor department path vo

--- DIrectoryService/src/DirectoryService.Domain/Departments/ValueObjects/Path.cs
+++ DIrectoryService/src/DirectoryService.Domain/Departments/ValueObjects/Path.cs
@@ -6,6 +6,11 @@ public record Path
 {
     public Path(Identifier identifier, Department? parentDepartment)
     {
+        BuildDepartmentPath(identifier, parentDepartment);
+    }
+
+    private void BuildDepartmentPath(Identifier identifier, Department? parentDepartment)
+    {
         StringBuilder pathBuilder = new();
         pathBuilder.Insert(0, identifier.Value);
 
