6f8cc9758e9bfdc165915b6f10aa4f8f585fd57c
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-25 17:05:33 +0100

    add location migration

--- DIrectoryService/src/DirectoryService.Infrastructure/Persistence/Configuration/LocationConfiguration.cs
+++ DIrectoryService/src/DirectoryService.Infrastructure/Persistence/Configuration/LocationConfiguration.cs
@@ -10,8 +10,30 @@ public class LocationConfiguration : IEntityTypeConfiguration<Location>
     {
         builder.ToTable("locations");
         builder.HasKey(l => l.Id).HasName("id");
-        builder.Property(l => l.Name.Value)
+        
+        builder.Property(l => l.Name)
+            .HasConversion(l => l.Value, l => Name.Create(l).Value)
             .IsRequired()
             .HasMaxLength(Name.MAX_LENGHT);
+
+        builder.OwnsOne(l => l.Address, ab =>
+        {
+            ab.Property(a => a.City)
+                .HasColumnName("city")
+                .IsRequired();
+            ab.Property(a => a.Street)
+                .HasColumnName("street")
+                .IsRequired();
+            ab.Property(a => a.Building)
+                .HasColumnName("building")
+                .IsRequired();
+            ab.Property(a => a.Postcode)
+                .HasColumnName("postcode")
+                .IsRequired();
+        });
+
+        builder.Property(l => l.Timezone)
+            .HasConversion(l => l.Code, l => Timezone.Create(l).Value)
+            .IsRequired();
     }
 }
