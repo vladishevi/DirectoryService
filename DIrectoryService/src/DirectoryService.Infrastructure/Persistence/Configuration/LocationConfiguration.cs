da6aa38b868f3f9ae6165ae47e1ac6e0beb93cf3
vshevelevich <vladimir.shevelevich@gmail.com>
2026-03-26 16:02:19 +0100

    update location configuration and add migration

--- DIrectoryService/src/DirectoryService.Infrastructure/Persistence/Configuration/LocationConfiguration.cs
+++ DIrectoryService/src/DirectoryService.Infrastructure/Persistence/Configuration/LocationConfiguration.cs
@@ -9,31 +9,43 @@ public class LocationConfiguration : IEntityTypeConfiguration<Location>
     public void Configure(EntityTypeBuilder<Location> builder)
     {
         builder.ToTable("locations");
-        builder.HasKey(l => l.Id).HasName("id");
+        builder.HasKey(l => l.Id).HasName("pk_locations");
         
         builder.Property(l => l.Name)
+            .HasColumnName("name")
             .HasConversion(l => l.Value, l => Name.Create(l).Value)
             .IsRequired()
             .HasMaxLength(Name.MAX_LENGHT);
 
         builder.OwnsOne(l => l.Address, ab =>
         {
+            ab.ToJson("address");
+            
             ab.Property(a => a.City)
-                .HasColumnName("city")
                 .IsRequired();
             ab.Property(a => a.Street)
-                .HasColumnName("street")
                 .IsRequired();
             ab.Property(a => a.Building)
-                .HasColumnName("building")
                 .IsRequired();
             ab.Property(a => a.Postcode)
-                .HasColumnName("postcode")
                 .IsRequired();
         });
 
         builder.Property(l => l.Timezone)
+            .HasColumnName("timezone")
             .HasConversion(l => l.Code, l => Timezone.Create(l).Value)
+            .IsRequired();
+        
+        builder.Property(l => l.IsActive)
+            .HasColumnName("is_active")
+            .IsRequired();
+        
+        builder.Property(l => l.CreatedAt)
+            .HasColumnName("created_at")
+            .IsRequired();
+        
+        builder.Property(l => l.UpdatedAt)
+            .HasColumnName("updated_at")
             .IsRequired();
     }
 }
