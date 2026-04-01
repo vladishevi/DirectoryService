be47f6a9c05ab51d591b2112c2a72a13199c9925
vshevelevich <vladimir.shevelevich@gmail.com>
2026-04-01 10:40:13 +0200

    refactor program.cs. add DI extensions

---
+++ DIrectoryService/src/DIrectoryService.Presenters/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿namespace DirectoryService.Presenters;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+
+        return services;
+    }
+
+    public static WebApplication Configure(this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Directory Service"));
+        }
+
+        app.MapControllers();
+
+        return app;
+    }
+}
--- DIrectoryService/src/DIrectoryService.Presenters/Program.cs
+++ DIrectoryService/src/DIrectoryService.Presenters/Program.cs
@@ -1,5 +1,6 @@
 using System.Globalization;
 using DirectoryService.Infrastructure.Postgres;
+using DirectoryService.Presenters;
 using Serilog;
 
 Log.Logger = new LoggerConfiguration()
@@ -13,20 +14,12 @@ try
 
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-    builder.Services.AddControllers();
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-    builder.Services.AddPostgresInfrastructure(builder.Configuration);
-
+    builder.Services.AddApiServices()
+        .AddPostgresInfrastructure(builder.Configuration);
+    
     WebApplication app = builder.Build();
 
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Directory Service"));
-    }
-
-    app.MapControllers();
+    app.Configure();
 
     app.Run();
 }
