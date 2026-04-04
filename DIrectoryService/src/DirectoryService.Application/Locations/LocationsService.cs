0fec3cbb37e2ff7d291633ba10d0eaa07c4a02eb
vshevelevich <vladimir.shevelevich@gmail.com>
2026-04-04 16:23:58 +0200

    add initial locations classes

---
+++ DIrectoryService/src/DirectoryService.Application/Locations/LocationsService.cs
@@ -0,0 +1,33 @@
+﻿using DirectoryService.Contracts.Locations;
+using DirectoryService.Domain.Locations;
+
+namespace DirectoryService.Application.Locations;
+
+/// <summary>
+/// 
+/// </summary>
+public class LocationsService
+{
+    private readonly ILocationsRepository _locationsRepository;
+
+    public LocationsService(ILocationsRepository locationsRepository)
+    {
+        _locationsRepository = locationsRepository;
+    }
+
+    public async Task Create(CreateLocationDto locationDto, CancellationToken cancellationToken)
+    {
+        //проверка валидности
+        //создание сущности
+        Name name = Name.Create(locationDto.Name).Value;
+        Address address = Address
+            .Create(locationDto.City, locationDto.Street, locationDto.Building, locationDto.Postcode).Value;
+        Timezone timezone = Timezone.Create(locationDto.Timezone).Value;
+        
+        Location location = new Location(name, address, timezone);
+        
+        //сохранение в бд
+        await _locationsRepository.AddAsync(location, cancellationToken);
+        //логирование
+    }
+}
---
+++ DIrectoryService/src/DirectoryService.Contracts/Locations/CreateLocationDto.cs
@@ -0,0 +1,3 @@
+﻿namespace DirectoryService.Contracts.Locations;
+
+public record CreateLocationDto(string Name, string City, string Street, int Building, string Postcode, string Timezone);
