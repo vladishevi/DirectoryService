12f02120510835f1202ededb7863baf9a31ba068
vshevelevich <vladimir.shevelevich@gmail.com>
2026-04-13 11:49:43 +0200

    add envelop class

---
+++ DIrectoryService/src/DIrectoryService.Presenters/Envelope.cs
@@ -0,0 +1,19 @@
+﻿using Shared;
+
+namespace DirectoryService.Presenters;
+
+public record Envelope
+{
+    private Envelope(object? result, Errors? errors)
+    {
+        Result = result;
+        Errors = errors;
+    }
+    
+    public Object Result { get; }
+    public Errors Errors { get; }
+    public DateTime Timestamp { get; } = DateTime.UtcNow;
+    
+    public static Envelope Ok(object? result = null) => new Envelope(result, null);
+    public static Envelope Error(Errors errors) => new Envelope(null, errors);
+}
