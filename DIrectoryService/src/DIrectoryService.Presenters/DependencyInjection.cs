0ab138a7c7b51eb686b12f7469ae5a8d9e8af5cc
vshevelevich <vladimir.shevelevich@gmail.com>
2026-06-09 12:11:24 +0200

    Restore HTTP logging middleware

--- DIrectoryService/src/DIrectoryService.Presenters/DependencyInjection.cs
+++ DIrectoryService/src/DIrectoryService.Presenters/DependencyInjection.cs
@@ -35,6 +35,7 @@ public static class DependencyInjection
     {
         app.UseExceptionMiddleware();
         app.UseSerilogRequestLogging();
+        app.UseHttpLogging();
         Dapper.DefaultTypeMap.MatchNamesWithUnderscores = true;
         
         if (app.Environment.IsDevelopment())
