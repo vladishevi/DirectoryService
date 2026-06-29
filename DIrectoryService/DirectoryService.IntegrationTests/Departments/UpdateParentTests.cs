b7acb305f46ec9bb8e76dc782c3f301b26f69638
vshevelevich <vladimir.shevelevich@gmail.com>
2026-06-29 16:05:53 +0200

    add new add_parent test

--- DIrectoryService/DirectoryService.IntegrationTests/Departments/UpdateParentTests.cs
+++ DIrectoryService/DirectoryService.IntegrationTests/Departments/UpdateParentTests.cs
@@ -11,7 +11,7 @@ namespace DirectoryService.IntegrationTests.Departments;
 public class UpdateParentTests(DirectoryServiceTestWebFactory factory) : DirectoryServiceTests(factory)
 {
     [Fact]
-    public async Task UpdateParent_with_a_parent_to_null_should_succeed()
+    public async Task UpdateParent_set_parent_to_null_should_succeed()
     {
         //arrange
         //create locations
@@ -65,5 +65,63 @@ public class UpdateParentTests(DirectoryServiceTestWebFactory factory) : Directo
         Assert.Null(updateParentEnvelope.Errors);
         Assert.Null(department.ParentDepartment);
         Assert.Equal(department.Depth, 0);
+    }
+    
+    [Fact]
+    public async Task UpdateParent_add_parent_should_succeed()
+    {
+        //arrange
+        //create locations
+        var request = new CreateLocationRequest("My locfatgion test",
+            new AddressDto { City = "my city", Street = "my strgeet", Building = 1, Postcode = "92" }, "Europe/London");
+        var createLocationEnvelope =
+            await Client.PostAndReadAsJsonAsync<Envelope<Guid>, CreateLocationRequest>("api/locations", request);
+        var firstLocationId = createLocationEnvelope.Result;
+        
+        request = new CreateLocationRequest("My locfatgion test second",
+            new AddressDto { City = "my city", Street = "my second strgeet", Building = 1, Postcode = "92" }, "Europe/London");
+        createLocationEnvelope =
+            await Client.PostAndReadAsJsonAsync<Envelope<Guid>, CreateLocationRequest>("api/locations", request);
+        var secondLocationId = createLocationEnvelope.Result;
+        
+        //create parent department
+        var createParentDepartmentRequest = new CreateDepartmentRequest(Name: "Parent Department",
+            Identifier: "parent", ParentId: null, LocationIds: [firstLocationId]);
+        var createParentEnvelope =
+            await Client.PostAndReadAsJsonAsync<Envelope<Guid>, CreateDepartmentRequest>("api/departments", createParentDepartmentRequest);
+        var parentId = createParentEnvelope.Result;
+        
+        //create department
+        var createDepartmentRequest = new CreateDepartmentRequest(Name: "Test Department",
+            Identifier: "test", null, LocationIds: [secondLocationId]);
+        var createDepartmentEnvelope = await Client.PostAndReadAsJsonAsync<Envelope<Guid>, CreateDepartmentRequest>("api/departments",
+            createDepartmentRequest);
+        var departmentId = createDepartmentEnvelope.Result;
+        var department = await ExecuteInDbAsync(async dbContext =>
+        {
+            return await dbContext.DepartmentsRead
+                .Include(d => d.ParentDepartment)
+                .FirstAsync(d => d.Id == departmentId);
+        });
+
+        var updateParentRequest = new UpdateParentRequest(createParentEnvelope.Result);
+
+        //act
+        var response = await Client.PutAsJsonAsync($"api/departments/{department.Id}/parent", updateParentRequest);
+        var updateParentEnvelope = await response.Content.ReadFromJsonAsync<Envelope<Guid>>();
+        department = await ExecuteInDbAsync(async dbContext =>
+        {
+            return await dbContext.DepartmentsRead
+                .Include(d => d.ParentDepartment)
+                .FirstAsync(d => d.Id == departmentId);
+        });
+        
+        //assert
+        Assert.True(updateParentEnvelope.IsSuccess);
+        Assert.True(response.IsSuccessStatusCode);
+        Assert.Null(updateParentEnvelope.Errors);
+        Assert.NotNull(department.ParentDepartment);
+        Assert.NotEqual(department.Depth, 0);
+        Assert.True(department.ParentDepartment.Id == parentId);
     }
 }
